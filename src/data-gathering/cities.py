e05b9335949a38a3648f36bde034cc291ef6a0fb
Milind Singh <singhmilind604@gmail.com>
2026-07-28 15:46:11 +0530

    feat: add city coordinates module

---
+++ src/data-gathering/cities.py
@@ -0,0 +1,65 @@
+city_coords = {
+# North
+"Chandigarh": {"lat": 30.7333, "lon": 76.7794},
+"Shimla": {"lat": 31.1048, "lon": 77.1734},
+"Dehradun": {"lat": 30.3165, "lon": 78.0322},
+"Lucknow": {"lat": 26.8467, "lon": 80.9462},
+"Amritsar": {"lat": 31.6340, "lon": 74.8723},
+"Ludhiana": {"lat": 30.9010, "lon": 75.8573},
+"Jammu": {"lat": 32.7266, "lon": 74.8570},
+
+# NCR
+"Delhi": {"lat": 28.6139, "lon": 77.2090},
+"Gurugram": {"lat": 28.4595, "lon": 77.0266},
+"Noida": {"lat": 28.5355, "lon": 77.3910},
+"Faridabad": {"lat": 28.4089, "lon": 77.3178},
+"Ghaziabad": {"lat": 28.6692, "lon": 77.4538},
+"Greater Noida": {"lat": 28.4744, "lon": 77.5040},
+
+# West
+"Gandhinagar": {"lat": 23.2156, "lon": 72.6369},
+"Mumbai": {"lat": 19.0760, "lon": 72.8777},
+"Panaji": {"lat": 15.4909, "lon": 73.8278},
+"Ahmedabad": {"lat": 23.0225, "lon": 72.5714},
+"Pune": {"lat": 18.5204, "lon": 73.8567},
+"Surat": {"lat": 21.1702, "lon": 72.8311},
+"Vadodara": {"lat": 22.3072, "lon": 73.1812},
+"Nagpur": {"lat": 21.1458, "lon": 79.0882},
+
+# South
+"Amaravati": {"lat": 16.5412, "lon": 80.5154},
+"Bengaluru": {"lat": 12.9716, "lon": 77.5946},
+"Thiruvananthapuram": {"lat": 8.5241, "lon": 76.9366},
+"Chennai": {"lat": 13.0827, "lon": 80.2707},
+"Hyderabad": {"lat": 17.3850, "lon": 78.4867},
+"Kochi": {"lat": 9.9312, "lon": 76.2673},
+"Coimbatore": {"lat": 11.0168, "lon": 76.9558},
+"Visakhapatnam": {"lat": 17.6868, "lon": 83.2185},
+
+# Central
+"Bhopal": {"lat": 23.2599, "lon": 77.4126},
+"Raipur": {"lat": 21.2444, "lon": 81.6306},
+"Indore": {"lat": 22.7196, "lon": 75.8577},
+
+# East
+"Patna": {"lat": 25.5941, "lon": 85.1376},
+"Ranchi": {"lat": 23.3600, "lon": 85.3300},
+"Bhubaneswar": {"lat": 20.2961, "lon": 85.8245},
+"Kolkata": {"lat": 22.5726, "lon": 88.3639},
+"Dhanbad": {"lat": 23.7957, "lon": 86.4304},
+"Siliguri": {"lat": 26.7271, "lon": 88.3953},
+
+# Northeast
+"Itanagar": {"lat": 27.1004, "lon": 93.6166},
+"Dispur": {"lat": 26.1445, "lon": 91.7362},
+"Imphal": {"lat": 24.8170, "lon": 93.9368},
+"Shillong": {"lat": 25.5788, "lon": 91.8933},
+"Aizawl": {"lat": 23.7271, "lon": 92.7176},
+"Kohima": {"lat": 25.6701, "lon": 94.1077},
+"Gangtok": {"lat": 27.3389, "lon": 88.6065},
+"Agartala": {"lat": 23.8315, "lon": 91.2868},
+"Guwahati": {"lat": 26.1445, "lon": 91.7362},
+
+# UT
+"Leh": {"lat": 34.1526, "lon": 77.5771},
+}
